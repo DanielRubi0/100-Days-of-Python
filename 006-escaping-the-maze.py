7da5f75b2398dfc69ab46aede2b2d497dbf969f5
Daniel Rubio García <114470415+DanielRubi0@users.noreply.github.com>
2023-04-12 10:28:41 +0200

    Day 6

--- 006-escaping-the-maze.py
+++ 006-escaping-the-maze.py
@@ -1,17 +1,17 @@
 # https://reeborg.ca/index_en.html
 
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
 
-while front_is_clear():
-    move()
-    turn_left()
+# while front_is_clear():
+#     move()
+#     turn_left()
 
-while not at_goal():
-    if right_is_clear():
-        turn_right()
-        move()         
-    else:
-        turn_left()
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()         
+#     else:
+#         turn_left()
